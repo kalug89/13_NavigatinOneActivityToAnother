f86efe618875d9a836de74688ec6b4a6a34d13cd
kalug89 <kaluzny_david@hotmail.com>
2018-08-19 20:57:20 +0200

    Add activities and inent to scond activity

--- app/src/main/java/pl/dkaluzny/navigatinoneactivitytoanother/MainActivity.kt
+++ app/src/main/java/pl/dkaluzny/navigatinoneactivitytoanother/MainActivity.kt
@@ -1,12 +1,22 @@
 package pl.dkaluzny.navigatinoneactivitytoanother
 
+import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        goSecondId.setOnClickListener {
+
+            //            var intent = Intent(this, SecondActivity::class.java)
+            //            startActivity(intent)
+            startActivity(Intent(this, SecondActivity::class.java))
+
+        }
     }
 }
