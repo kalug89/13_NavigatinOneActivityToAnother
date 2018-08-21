07a7a321c961750c066a624798c56d7fa5c8a9d4
kalug89 <kaluzny_david@hotmail.com>
2018-08-21 20:40:38 +0200

    Add link from one to two activity

--- app/src/main/java/pl/dkaluzny/navigatinoneactivitytoanother/MainActivity.kt
+++ app/src/main/java/pl/dkaluzny/navigatinoneactivitytoanother/MainActivity.kt
@@ -1,11 +1,14 @@
 package pl.dkaluzny.navigatinoneactivitytoanother
 
+import android.app.Activity
 import android.content.Intent
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    val REQUEST_CODE = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -18,7 +21,19 @@ class MainActivity : AppCompatActivity() {
             intent.putExtra("char", 'C')
             intent.putExtra("Int", 23)
 
-            startActivity(intent)
+            startActivityForResult(intent, REQUEST_CODE)
         }
     }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        if (requestCode == REQUEST_CODE){
+            if (resultCode == Activity.RESULT_OK) {
+                var result = data!!.extras.get("return")
+
+                Toast.makeText(this, result.toString(), Toast.LENGTH_LONG).show()
+            }
+        }
+    }
+
 }
--- app/src/main/java/pl/dkaluzny/navigatinoneactivitytoanother/SecondActivity.kt
+++ app/src/main/java/pl/dkaluzny/navigatinoneactivitytoanother/SecondActivity.kt
@@ -1,8 +1,10 @@
 package pl.dkaluzny.navigatinoneactivitytoanother
 
+import android.app.Activity
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_second.*
 
 class SecondActivity : AppCompatActivity() {
 
@@ -18,6 +20,13 @@ class SecondActivity : AppCompatActivity() {
             var int = data.getInt("Int")
 
             Toast.makeText(this, name.toString() + ", char: " + char + ", int: "+ int, Toast.LENGTH_LONG).show()
+        }
+
+        goBackButton.setOnClickListener{
+            var returnIntent = this.intent
+            returnIntent.putExtra("return", "Hello from Second Activity")
+            setResult(Activity.RESULT_OK, returnIntent)
+            finish()
         }
     }
 }
